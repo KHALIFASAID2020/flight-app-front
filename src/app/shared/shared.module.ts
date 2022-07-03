622fb687e796e9a942ad85cf7433afc5d6870e50
KHALIFA SAID <khalifasaid.2020@gmail.com>
2022-07-03 18:29:00 +0200

    add new component shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFormComponent } from './components/search-form/search-form.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatSelectModule} from "@angular/material/select";
 
 
 
@@ -9,7 +12,8 @@ import { SearchFormComponent } from './components/search-form/search-form.compon
     SearchFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSelectModule
   ],
   exports: [
     SearchFormComponent
